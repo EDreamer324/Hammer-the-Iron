9f46803e1644b1685af030667631b2f3f17ed84d
EDreamer324 <artyomksenzov@gmail.com>
2023-12-12 15:24:51 +0100

    Socket Interactor
    Basic socket interactor functional

---
+++ Assets/Hammer the Iron/Scripts/Smelting.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smelting : MonoBehaviour
+{
+    [SerializeField] private GameObject ingot;
+
+    private void OnTriggerEnter()
+    {
+        if (!ingot)
+        {
+            
+        }
+    }
+}
