01b7af4fe5443839519e135c51a86949f3d69f53
EDreamer324 <artyomksenzov@gmail.com>
2023-12-13 13:25:12 +0100

    metals heat up
    time frame for process in progress

--- Assets/Hammer the Iron/Scripts/Smelting.cs
+++ Assets/Hammer the Iron/Scripts/Smelting.cs
@@ -1,16 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Smelting : MonoBehaviour
 {
-    [SerializeField] private GameObject ingot;
+    //Variables used by the color shifting function
+    [SerializeField] private Material material;
+    [SerializeField] private Color[] colors;
+    private int startingColorIndex = 0;
+    private int targetColorIndex = 1;
+    private float currentColorIndex;
 
-    private void OnTriggerEnter()
+    private void OnTriggerEnter(Collider other)
     {
-        if (!ingot)
+        material = other.GetComponent<Renderer>().material;
+        //colors.Add(material.GetColor("_Color"));
+        MetalHeatingUp();
+    }
+
+    private void OnTriggerExit()
+    {
+        MetalCoolingDown();
+    }
+
+    /// <summary>
+    /// Shifts the color of the game object to the set color
+    /// </summary>
+    private void MetalHeatingUp()
+    {
+        //while (currentColorIndex < 1)
+        //{
+            currentColorIndex += Time.fixedTime;
+            material.color = Color.Lerp(colors[startingColorIndex], colors[targetColorIndex], currentColorIndex);
+        //}
+    }
+
+    /// <summary>
+    /// Shifts the color of the game object back to the original color
+    /// </summary>
+    private void MetalCoolingDown()
+    {
+        while (currentColorIndex > 0)
         {
-            
+            currentColorIndex -= Time.deltaTime;
+            material.color = Color.Lerp(colors[startingColorIndex], colors[targetColorIndex], currentColorIndex);
         }
     }
 }
